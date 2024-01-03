22cc1cc2b2914d98732a608bad91502f0340238a
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2024-01-03 20:07:18 -0300

    refactor(CityControllerIT): add content property and sort param

--- backend/src/test/java/com/devsuperior/bds02/controllers/CityControllerIT.java
+++ backend/src/test/java/com/devsuperior/bds02/controllers/CityControllerIT.java
@@ -34,13 +34,13 @@ public class CityControllerIT {
 	public void findAllShouldReturnAllResourcesSortedByName() throws Exception {
 		
 		ResultActions result =
-				mockMvc.perform(get("/cities")
+				mockMvc.perform(get("/cities?sort=name")
 					.contentType(MediaType.APPLICATION_JSON));
 
 		result.andExpect(status().isOk());
-		result.andExpect(jsonPath("$[0].name").value("Belo Horizonte"));
-		result.andExpect(jsonPath("$[1].name").value("Belém"));
-		result.andExpect(jsonPath("$[2].name").value("Brasília"));
+		result.andExpect(jsonPath("$.content[0].name").value("Belo Horizonte"));
+		result.andExpect(jsonPath("$.content[1].name").value("Belém"));
+		result.andExpect(jsonPath("$.content[2].name").value("Brasília"));
 	}
 	
 	@Test
